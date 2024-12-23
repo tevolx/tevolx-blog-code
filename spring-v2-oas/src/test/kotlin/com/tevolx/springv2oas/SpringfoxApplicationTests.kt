e671ffdea5528862541dbbf8b726cc21c8493da0
Stuart Park <tevolx0@gmail.com>
2024-12-24 00:06:12 +0900

    Spring v2 oas 추가 (#4)

---
+++ spring-v2-oas/src/test/kotlin/com/tevolx/springv2oas/SpringfoxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tevolx.springv2oas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringfoxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
